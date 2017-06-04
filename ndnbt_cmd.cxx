5ef2e279e89a2a757308a376c1e4663acdab0490
Da Teng <da_teng0702@hotmail.com>
2017-06-04 14:29:43 -0700

    Add nfdc command support

--- ndnbt_cmd.cxx
+++ ndnbt_cmd.cxx
@@ -1,4 +1,9 @@
+#include <cstdio>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
 extern "C" {
 #include "cmd_emulate/cmd_emulate.h"
 }
@@ -92,15 +97,29 @@ run_producer()
   return 0;
 }
 
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != NULL)
+            result += buffer.data();
+    }
+    return result;
+}
+
 /**
  *  Input handler function for the interactive shell
  */
 void
 rl_handler(char *in)
 {
-  printf("You just entered\n");
-  printf("%s\n", in);
+  printf("command: %s\n", in);
   int err = 0;
+  char* in_orig;
+  in_orig = strdup(in);
+
   // If it's the quit user signal
   if (strcmp(in, g_signals[CMDLINE_SIG_QUIT]) == 0) {
     // rage quit!
@@ -108,15 +127,44 @@ rl_handler(char *in)
   }
 
   // Initialize producer
-  if (strcmp(in, g_signals[CMDLINE_SIG_RUN_PRODUCER]) == 0) {
+  //if (strcmp(in, g_signals[CMDLINE_SIG_RUN_PRODUCER]) == 0) {
+  //
+  //  err = run_producer();
+  //  std::cout << "run_producer Error: " << err << std::endl;
+  //}
 
-    err = run_producer();
-    std::cout << "run_producer Error: " << err << std::endl;
+  add_history(in);
+
+  char *cmd, *arg;
+  cmd = strtok_r(in, " ", &arg);
+
+  if (!cmd)
+    return;
+
+  if (arg) {
+		int len = strlen(arg);
+		if (len > 0 && arg[len - 1] == ' ')
+			arg[len - 1] = '\0';
+	}
+
+  // std::cout << "cmd: " << cmd << std::endl;
+  // std::cout << "arg: " << arg << std::endl;
+
+
+  if (strcmp(cmd, "face") == 0) {
+    std::string nfdc_out;
+    std::string nfdc_exec("nfdc ");
+    std::string nfdc_arg1(cmd);
+    std::string nfdc_arg2(arg);
+    std::string arg_list = nfdc_arg1 + " " + nfdc_arg2;
+    std::string nfdc_cmd = nfdc_exec + arg_list;
+    nfdc_out = exec(nfdc_cmd.c_str());
+    std::cout << nfdc_out << std::endl;
+    return;
   }
 
   // Using bluetoothctl command handler
-  bt_handler(in);
-
+  bt_handler(in_orig);
 }
 
 namespace ndn {
